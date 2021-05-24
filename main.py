07517a3463ade8b7c74e1964152769e2a1daa3d2
hhcodenext <84354968+hhcodenext@users.noreply.github.com>
2021-05-24 14:24:00 -0700

    Update main.py
    Changed indentation, os.system command, etc. in order to actually work on the Raspberry Pi.

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ sense = SenseHat()
 
 # While loop that prints the current time (local)
 while True:
-  t = time.localtime()
-  print(str(t.tm_hour)+":"+str(t.tm_min)+":"+str(t.tm_sec))
-  time.sleep(.1)
-  os.system('cls')
+    t = time.localtime()
+    print(str(t.tm_hour)+":"+str(t.tm_min)+":"+str(t.tm_sec))
+    time.sleep(.1)
+    # os.system('clear') # replace with 'cls' if not on sense-hat
   
-  # Some stuff to detect movement
-  acceleration = sense.get_accelerometer_raw()
-  x = acceleration['x']
-  y = acceleration['y']
-  z = acceleration['z']
+    # Some stuff to detect movement
+    acceleration = sense.get_accelerometer_raw()
+    x = acceleration['x']
+    y = acceleration['y']
+    z = acceleration['z']
 
   x=abs(x)
   y=abs(y)
   z=abs(z)
-	
+  
   if x > 1 or y > 1 or z > 1:
     sense.show_letter("!")
+    break
   else:
     sense.clear()
