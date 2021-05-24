02ad5bcb37a8810765f24681d00a3b441a494acb
hhcodenext <84354968+hhcodenext@users.noreply.github.com>
2021-05-24 17:29:24 -0500

    Update main.py
    Added some comments

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 import os
 import pygame
 
+# i is so that the noise only runs once
 i = 0
+# Initializing & loading alarm clock noise.
 pygame.mixer.init()
 pygame.mixer.music.load('noise.wav')
 
@@ -11,6 +13,8 @@ sense = SenseHat()
 
 sense.clear()
 
+# Function called when alarm clock is turned off
+# Referenced in the while loop
 def turn_off_alarm():
     global i
     sense.show_letter("!", text_colour=(255,0,0))
@@ -19,7 +23,9 @@ def turn_off_alarm():
     time.sleep(1)
     alarm_time = input("Please enter a new time: ")
     sense.clear()
-        
+
+# Function called when alarm clock is set off
+# Referenced in the while loop
 def set_off_alarm():
     global i
     sense.clear((255,255,255))
@@ -29,19 +35,21 @@ def set_off_alarm():
     if i == 1:
         pygame.mixer.music.play()
 
+# Input from the user for the alarm time
 alarm_time = input("Please print what time you would like the alarm to go off (Format: hh:mm): ")
 
-# While loop to check time.
+# While loop to check alarm.
 while True:
     t = time.localtime()
     # print(str(t.tm_hour)+":"+str(t.tm_min)+":"+str(t.tm_sec))
     # ^^FOR TESTING^^
     time.sleep(.1)
     
+    # Formatting the localtime & checking the alarm
     time_formatted = str(t.tm_hour)+":"+str(t.tm_min)
-    alarm_off = time_formatted == alarm_time    
+    alarm_on = time_formatted == alarm_time    
     
-    if alarm_off:
+    if alarm_on:
         set_off_alarm()
     
     # Some stuff to detect movement
@@ -54,10 +62,11 @@ while True:
     y=abs(y)
     z=abs(z)
   
+    # Logic for all the stuff above
     if x > 1 or y > 1 or z > 1:
         turn_off_alarm()
         continue
-    if alarm_off:
+    if alarm_on:
         continue
     else:
         sense.clear()
