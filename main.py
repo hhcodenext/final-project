7a23bd54467c19019d01bd8b6cba1ebfd9e591cc
hhcodenext <84354968+hhcodenext@users.noreply.github.com>
2021-05-24 16:48:28 -0500

    Update main.py
    Added some logic for setting & setting off the alarm, along with turning it off & setting a new one when turned off. Put all of that into functions in order to clean it up.

--- main.py
+++ main.py
@@ -4,25 +4,46 @@ import os
 
 sense = SenseHat()
 
-# While loop that prints the current time (local)
+sense.clear()
+
+def turn_off_alarm():
+    sense.show_letter("!", text_colour=(255,0,0))
+        time.sleep(1)
+        alarm_time = input("Please enter a new time: ")
+        sense.clear()
+       
+def set_off_alarm():
+    sense.clear((255,255,255))
+
+alarm_time = input("Please print what time you would like the alarm to go off (Format: hh:mm): ")
+
+# While loop to check time.
 while True:
     t = time.localtime()
-    print(str(t.tm_hour)+":"+str(t.tm_min)+":"+str(t.tm_sec))
+    # print(str(t.tm_hour)+":"+str(t.tm_min)+":"+str(t.tm_sec))
+    # ^^FOR TESTING^^
     time.sleep(.1)
-    # os.system('clear') # replace with 'cls' if not on sense-hat
-  
+   
+    time_formatted = str(t.tm_hour)+":"+str(t.tm_min)
+    alarm_off = time_formatted == alarm_time    
+   
+    if alarm_off:
+        set_off_alarm()
+   
     # Some stuff to detect movement
     acceleration = sense.get_accelerometer_raw()
     x = acceleration['x']
     y = acceleration['y']
     z = acceleration['z']
 
-  x=abs(x)
-  y=abs(y)
-  z=abs(z)
-  
-  if x > 1 or y > 1 or z > 1:
-    sense.show_letter("!")
-    break
-  else:
-    sense.clear()
+    x=abs(x)
+    y=abs(y)
+    z=abs(z)
+ 
+    if x > 1 or y > 1 or z > 1:
+        turn_off_alarm()
+        continue
+    if alarm_off:
+        continue
+    else:
+        sense.clear()
